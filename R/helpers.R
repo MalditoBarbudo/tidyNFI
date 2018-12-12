f12cf9a8ed1064839849418e5befee4ed4640d45
MalditoBarbudo <victorgrandagarcia@gmail.com>
2018-12-12 12:54:14 +0100

    added custom_poly_to_sf and custom_polygon_filter_expr functions. No tests yet

--- R/helpers.R
+++ R/helpers.R
@@ -114,26 +114,52 @@ nif_get_scenario <- function(viz_shape, agg_level) {
   return(scenario)
 }
 
+#' sf builder for leaflet custom polygons plugin
+#'
+#' Helper to create sf objects from leaflet polygons
+#'
+#' @param custom_polygon object as the one returned by the leaflet plugin
+custom_poly_to_sf <- function(custom_polygon) {
+  custom_polygon[['geometry']][['coordinates']] %>%
+    purrr::flatten() %>%
+    purrr::modify_depth(1, purrr::set_names, nm = c('long', 'lat')) %>%
+    dplyr::bind_rows() %>%
+    {list(as.matrix(.))} %>%
+    sf::st_polygon() %>%
+    sf::st_sfc() %>%
+    sf::st_sf(crs = "+proj=longlat +datum=WGS84")
+}
+
 #' Custom polygon filter expression builder
 #'
 #' @param conn pool object with the database connection
-#' @param custom_polygon polygon object as the one returned by the leaflet plugin
-custom_polygon_filter_expr <- function(conn, custom_polygon) {
+#' @param custom_polygon sf or sp object with the custom polygon to filter plots by
+custom_polygon_filter_expr <- function(custom_polygon, conn) {
 
-  ### TODO convert this into a function, and then, this one can accept spatialpolygons
-  ### objects to make it general ;)
-  tmp <- custom_polygon[['geometry']][['coordinates']] %>%
-    purrr::flatten() %>%
-    purrr::set_names(nm = 1:length(.)) %>%
-    purrr::modify_depth(1, purrr::set_names, nm = c('long', 'lat')) %>%
-    dplyr::bind_rows() %>%
-    sp::Polygon() %>%
-    sp::SpatialPolygons(list(sp::Polygons(list(.), 'custom_polygon')))
+  # if sp, then to sf
+  if (class(custom_polygon) == 'Polygons') {
+    custom_polygon <- sf::st_as_sf(custom_polygon)
+  }
 
-  dpyr::tbl(conn, 'PLOTS') %>%
-    dplyr::select(coords_longitude, coords_latitude) %>%
-    sp::SpatialPoints() %>%
-    sp::over(tmp) %>%
+  plots_tmp <- dplyr::tbl(conn, 'PLOTS') %>%
+    dplyr::select(plot_id, coords_longitude, coords_latitude) %>%
+    dplyr::collect()
 
+  plots_tmp %>% sf::st_as_sf(
+    coords = c('coords_longitude', 'coords_latitude'),
+    crs = "+proj=longlat +datum=WGS84"
+  ) %>%
+    sf::st_intersects(custom_polygon, sparse = FALSE) %>%
+    which() -> points_inside_index
+
+  plots_tmp %>%
+    dplyr::pull(plot_id) %>%
+    magrittr::extract(points_inside_index) -> plots_inside_polygon
+
+  filter_expression <- rlang::quo(
+    plot_id %in% !! plots_inside_polygon
+  )
+
+  return(filter_expression)
 
 }
